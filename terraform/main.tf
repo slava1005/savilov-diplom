b86257b3d34514dc3b0ad32f32b146a9c3e6868c
VJACHESLAV <114395964+slava1005@users.noreply.github.com>
2024-12-17 18:26:18 +0300

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -6,17 +6,15 @@ terraform {
   }
   backend "s3" {
   endpoint = "storage.yandexcloud.net"
-    }
     bucket = "savilovvv"
     region = "ru-central1"
     key    = "my-tf-state.tfstate"
 
        skip_region_validation      = true
        skip_credentials_validation = true
-       skip_requesting_account_id  = true # Необходимая опция Terraform для версии 1.6.1 и старше.
-       skip_s3_checksum            = true # Необходимая опция при описании бэкенда для Terraform версии 1.6.3 и старше.
+#      skip_requesting_account_id  = true # Необходимая опция Terraform для версии 1.6.1 и старше.
+#      skip_s3_checksum            = true # Необходимая опция при описании бэкенда для Terraform версии 1.6.3 и старше.
   }
-}
 
 provider "yandex" {
   token     = var.token
