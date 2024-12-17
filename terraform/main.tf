49c6403fcb6b39420fe8db8969bd932ca748a479
slava1005 <slava100575@gmail.com>
2024-12-17 18:15:51 +0300

    Изменил endpoint

--- terraform/main.tf
+++ terraform/main.tf
@@ -5,17 +5,18 @@ terraform {
     }
   }
   backend "s3" {
-    endpoints = {
-      s3 = "https://storage.yandexcloud.net"
-    }
+   endpoint = "storage.yandexcloud.net" 
+#   endpoints = {
+ #     s3 = "https://storage.yandexcloud.net"
+ #   }
     bucket = "savilovvv"
     region = "ru-central1"
     key    = "my-tf-state.tfstate"
 
        skip_region_validation      = true
        skip_credentials_validation = true
-       skip_requesting_account_id  = true # Необходимая опция Terraform для версии 1.6.1 и старше.
-       skip_s3_checksum            = true # Необходимая опция при описании бэкенда для Terraform версии 1.6.3 и старше.
+#       skip_requesting_account_id  = true # Необходимая опция Terraform для версии 1.6.1 и старше.
+#       skip_s3_checksum            = true # Необходимая опция при описании бэкенда для Terraform версии 1.6.3 и старше.
   }
 }
 
